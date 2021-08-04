79387084d1e737020a393321014dafaa7075ebb0
jessiLeandro <jessi_leandro@hotmail.com>
2021-08-04 11:40:17 -0300

    Chore: get on api locations

--- src/Containers/Vehicle/Manager/Map/index.js
+++ src/Containers/Vehicle/Manager/Map/index.js
@@ -1,37 +1,65 @@
-import React from 'react'
-import { MapContainer,TileLayer, Marker, Popup } from 'react-leaflet'
+import React, { useEffect, useState } from 'react'
+import { MapContainer,TileLayer, Marker, Popup, Circle, CircleMarker } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import leaflet from "leaflet";
+import useGeolocation from 'react-hook-geolocation'
+import { map } from 'ramda';
 
-const MyMap = () => {
-  const position = [51.505, -0.09]
-  const stylesMapBox = [
-    "light-v10",
-    "dark-v10",
-    "streets-v11",
-    "outdoors-v11",
-    "satellite-v9",
-    "satellite-streets-v11",
-    "navigation-preview-day-v4",
-    "navigation-preview-night-v4",
-    "navigation-guidance-day-v4",
-    "navigation-guidance-night-v4",
-  ];
+import truckPin from './truckPin.png'
+import { getAllGeoLocation } from '../../../../Services/Vehicle';
+
+const mapIcon = leaflet.icon({
+  iconUrl: truckPin,
+  iconSize: [50, 50],
+  iconAnchor: [25, 50],
+  popupAnchor: [0, -50],
+});
+
+const MyMap = ({ }) => {
+  const position= [-23.7056163, -46.5404382]
+  const geolocation = useGeolocation()
+  const [vehicles, setVehicles] = useState([])
+
+  
+  useEffect(() => {
+    getAllGeoLocation().then(({ data }) => {
+      setVehicles(data.rows);
+    })
+  }, [])
+
+
+  if(!geolocation.latitude && !geolocation.error ){
+    return <h1>loading...</h1>
+  }
+
+  const center = geolocation.latitude ? [geolocation.latitude, geolocation.longitude] : position
 
   return (
     <MapContainer
-      center={position}
+      center={center}
       zoom={13}
       scrollWheelZoom={false}
       style={{ width: "100%", height: "500px" }}
     >
     <TileLayer
-      url={`https://api.mapbox.com/styles/v1/mapbox/${stylesMapBox[3]}/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+      url={`https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
     /> 
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker> 
+    {map((vehicle) => {
+      if(vehicle.tracks?.length === 0) return
+
+      const track = vehicle.tracks[0]
+
+      return (
+        <Marker position={[track.gpsLatitude, track.gpsLongitude]} icon={mapIcon}>
+          <Popup closeButton={false}>
+            {vehicle.plate}
+          </Popup>
+        </Marker>
+        )
+      }, vehicles)}
+
+    {/* <Circle center={center} radius={10} /> */}
+    <CircleMarker center={center} radius={2} />
   </MapContainer>
   )
 }
