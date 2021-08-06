a6fbbc0932f8b6a9e9074e9ba058b9332f83c32f
jessiLeandro <jessi_leandro@hotmail.com>
2021-08-06 13:49:30 -0300

    many changes

--- src/Pages/Mobile/DriverMobile/index.js
+++ src/Pages/Mobile/DriverMobile/index.js
@@ -1,11 +1,17 @@
+import { isEmpty } from 'ramda'
 import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
+import { message } from 'antd'
 
 import DriverMobileContainer from '../../../Containers/Mobile/DriverMobile'
+import { getAllAuthorizations } from '../../../Services/Authorization'
 import { getById, updateDriverWithoutAuth } from '../../../Services/Driver'
 
 const DriverMobile = ({ match }) => {
+  const [plate, setPlate] = useState(null)
   const [driver, setDriver] = useState(null)
+  const [authorizations, setAuthorizations] = useState([])
+  const [authorizationId, setAuthorizationId] = useState()
   const history = useHistory()
 
   useEffect(() => {
@@ -25,7 +31,44 @@ const DriverMobile = ({ match }) => {
     }
   }
 
-  return <DriverMobileContainer driver={driver} updatePhone={updatePhone} />
+  const getAuthorization = async (values) => {
+    setPlate(values.plate)
+    try {
+      const { data } = await getAllAuthorizations({
+        ...values,
+        driverId: match.params.id
+      })
+
+      if (isEmpty(data)) {
+        message.error('Não foi encontrado autorização para este veículo')
+      }
+
+      setAuthorizations(data)
+      if (data.length === 1) {
+        setAuthorizationId(data[0].id)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const restart = () => {
+    setAuthorizations([])
+    setAuthorizationId()
+    setPlate(null)
+  }
+
+  return (
+    <DriverMobileContainer
+      driver={driver}
+      updatePhone={updatePhone}
+      authorizations={authorizations}
+      getAuthorization={getAuthorization}
+      authorizationId={authorizationId}
+      plate={plate}
+      restart={restart}
+    />
+  )
 }
 
 export default DriverMobile
