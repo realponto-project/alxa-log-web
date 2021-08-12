4b140872e4e4d7f424a2c877c42adc90c1fa4b7c
Alexandre Soares <ale_santos.soares@hotmail.com>
2021-08-12 10:41:21 -0300

    chore: fix screen on detail vehicle

--- src/Containers/Vehicle/Detail/index.js
+++ src/Containers/Vehicle/Detail/index.js
@@ -6,7 +6,7 @@ import moment from 'moment'
 import { mapIcon } from '../../../Components/Map/Icons'
 import { length, pathOr } from 'ramda'
 
-const { Title, Paragraph } = Typography
+const { Title, Text } = Typography
 
 const renderSituation = (situation) => {
   return {
@@ -21,26 +21,19 @@ const Location = ({ tracks }) => {
   const latitude = pathOr(0, [0, 'gpsLatitude'], tracks)
   const longitude = pathOr(0, [0, 'gpsLongitude'], tracks)
   const position = [latitude, longitude]
-  const odometer = pathOr({}, [0, 'odometer'], tracks)
   return (
-    <Col span={24}>
-      <Card bordered={false}>
-        <MapContainer
-          center={position}
-          zoom={16}
-          style={{ width: '100%', height: 280 }}
-          dragging={false}
-          touchZoom={false}
-          zoomControl={false}
-          scrollWheelZoom={false}
-          doubleClickZoom={false}>
-          <TileLayer
-            url={`https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-          />
-          <Marker interactive={false} icon={mapIcon} position={position} />
-        </MapContainer>
-      </Card>
-    </Col>
+    <Card bordered={false} style={{ height: "330px" }}>
+      <MapContainer
+        center={position}
+        zoom={16}
+        style={{ width: '100%', height: 280 }}
+      >
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+        <Marker interactive={false} icon={mapIcon} position={position} />
+      </MapContainer>
+    </Card>
   )
 }
 
@@ -50,58 +43,87 @@ const Detail = ({ vehicle }) => {
 
   return (
     <Row gutter={[8, 8]}>
-      <Col span={24}>
-        <Card bordered={false}>
-          <Row gutter={[8, 20]}>
+      <Col span={8}>
+        <Location tracks={tracks} />
+      </Col>
+      <Col span={16}>
+        <Card bordered={false} style={{ height: "330px" }}>
+          <Row gutter={[8, 24]}>
             <Col span={24}>
               <Title level={4}>Detalhes</Title>
             </Col>
 
             <Col span={6}>
-              <Paragraph>Placa</Paragraph>
-              <Paragraph strong>{vehicle.plate}</Paragraph>
+              <Text>Placa</Text>
+              <br />
+              <Text>
+                <strong>{vehicle.plate}</strong>
+              </Text>
             </Col>
+
             <Col span={6}>
-              <Paragraph>Frota</Paragraph>
-              <Paragraph strong>{vehicle.fleet}</Paragraph>
+              <Text>Frota</Text>
+              <br />
+              <Text>
+                <strong>{vehicle.fleet}</strong>
+              </Text>
             </Col>
+
             <Col span={6}>
-              <Paragraph>Tipo de veículo</Paragraph>
-              <Paragraph strong>{vehicle?.vehicleType?.name ?? '-'}</Paragraph>
+              <Text>Tipo de veículo</Text>
+              <br />
+              <Text>
+                <strong>{vehicle?.vehicleType?.name ?? '-'}</strong>
+              </Text>
             </Col>
-            <Col span={3}>
-              <Paragraph>Situação</Paragraph>
-              <Paragraph strong>{renderSituation(vehicle.situation)}</Paragraph>
-            </Col>
-            <Col span={3}>
-              <Paragraph>Status</Paragraph>
-              <Paragraph strong>
-                {vehicle.activated ? (
+
+            <Col span={6}>
+              <Text>Status</Text>
+              <br />
+              <Text>
+                <strong>{vehicle.activated ? (
                   <Tag color="#268E86">Ativo</Tag>
                 ) : (
                   <Tag color="#EA5656">Inativo</Tag>
-                )}
-              </Paragraph>
+                )}</strong>
+              </Text>
             </Col>
+
             <Col span={6}>
-              <Paragraph>Número rastreador</Paragraph>
-              <Paragraph strong>{vehicle?.serialNumber ?? '-'}</Paragraph>
+              <Text>Situação</Text>
+              <br />
+              <Text>
+                <strong>{renderSituation(vehicle.situation)}</strong>
+              </Text>
             </Col>
+
             <Col span={6}>
-              <Paragraph>Km atual</Paragraph>
-              <Paragraph strong>{odometer} Km</Paragraph>
+              <Text>Número rastreador</Text>
+              <br />
+              <Text>
+                <strong>{vehicle?.serialNumber ?? '-'}</strong>
+              </Text>
             </Col>
-            <Col span={8}>
-              <Paragraph>Última manutenção</Paragraph>
-              <Paragraph strong>
-                {moment(vehicle.lastMaintenance).format('DD/MM/YYYY')}
-              </Paragraph>
+
+            <Col span={6}>
+              <Text>Km atual</Text>
+              <br />
+              <Text>
+                <strong>{odometer} Km</strong>
+              </Text>
+            </Col>
+
+            <Col span={6}>
+              <Text>Última manutenção</Text>
+              <br />
+              <Text>
+                <strong>{moment(vehicle.lastMaintenance).format('DD/MM/YYYY')}</strong>
+              </Text>
             </Col>
           </Row>
         </Card>
       </Col>
 
-      <Location tracks={tracks} />
     </Row>
   )
 }
