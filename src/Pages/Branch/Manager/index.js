a222b0e04c06ed77991d01f3e2f313316b48851d
jessiLeandro <jessi_leandro@hotmail.com>
2021-08-20 10:14:21 -0300

    chore: refactor manager branch

--- src/Pages/Branch/Manager/index.js
+++ src/Pages/Branch/Manager/index.js
@@ -2,90 +2,77 @@ import React, { useEffect, useState } from 'react'
 import { message } from 'antd'
 import { useLocation, withRouter } from 'react-router-dom'
 import { cnpj } from 'cpf-cnpj-validator'
-import { validateBr } from 'js-brasil'
+import qs from 'qs'
+
 import GAInitialize from '../../../utils/ga'
-
 import ManagerContainer from '../../../Containers/Branch/Manager'
-import {   
-  getAll, 
-  createBranch, 
-  updateBranch,  
-} from '../../../Services/Branch'
-import { isEmpty } from 'ramda'
+import { getAll, createBranch, updateBranch } from '../../../Services/Branch'
+import { add, isEmpty, pathOr, pipe } from 'ramda'
+import { parseQueryParams } from '../../../utils/queryParams'
 
-const Manager = ({
-  history,
-}) => {
+const success = (text) => {
+  message.success(text)
+}
+
+const errorMessage = (text) => {
+  message.error(text)
+}
+
+const Manager = ({ history }) => {
   const [branchData, setBranchData] = useState({ rows: [] })
   const [branchSelected, setbranchSelected] = useState(null)
-  const [searchValue, setSearchValue] = useState('')
-  const [offset, setoffset] = useState(1)
-
   const [loading, setLoading] = useState(true)
+  const [offset, setoffset] = useState(1)
+  const [searchValue, setSearchValue] = useState('')
   const { search, pathname } = useLocation()
-  GAInitialize(`/branch`)
-  useEffect(() => {
-    let query = { }
-    const searchLocaStorage = localStorage.getItem('branchSearch')
-    if(!search && searchLocaStorage) {
-      history.push({
-        pathname,
-        search: cnpj.isValid(searchLocaStorage) ? `?document=${searchLocaStorage}` : `?name=${searchLocaStorage}`
-      })
-      setSearchValue(localStorage.getItem('branchSearch'))
-      query = validateBr.cnh(searchValue) ? { document: searchLocaStorage.replace(/\D/g, '') } : { name: searchLocaStorage }
 
-    }
-    getBranchs(query)
-  }, [])
+  GAInitialize('/branch')
 
-  const success = (text) => {
-    message.success(text);
-  }
-  
-  const errorMessage = (text) => {
-    message.error(text)
+  const changeQueryParams = (search) => {
+    return history.push({
+      pathname,
+      search
+    })
   }
 
-  const getBranchs = async (params = {}) => {
+  const clearFilter = () => {
+    localStorage.removeItem('branchSearch')
+    setSearchValue('')
+
+    changeQueryParams('')
+  }
+
+  const getBranchs = async () => {
     setLoading(true)
     try {
-      const { data } = await getAll(params)
+      const { data } = await getAll(parseQueryParams(search))
       setBranchData(data)
       setLoading(false)
     } catch (error) {
       setLoading(false)
       window.onerror(`branchs: ${error.error}`, window.location.href)
-
     }
   }
 
-  const handleSubmit = async (values) => {
-    try {
-      await createBranch({...values, document: values.document.replace(/\D/g, '') })
-      getBranchs()
-      success('Cadastro da unidade realizado com sucesso!')
-    } catch (error) {
-      errorMessage('Não foi realizar o cadastro da unidade!')
-      window.onerror(`createBranch: ${error.error}`, window.location.href)
+  const handleChangeTableEvent = ({ current }) => {
+    const query = { offset: current - 1 }
+    const queryParams = parseQueryParams(search)
 
-    }
+    changeQueryParams(qs.stringify({ ...queryParams, ...query }))
   }
 
   const handleEdit = async (values) => {
     try {
-      await updateBranch({...values, document: values.document.replace(/\D/g, '') })
+      await updateBranch({
+        ...values,
+        document: values.document.replace(/\D/g, '')
+      })
       getBranchs()
       success('Editado unidade com sucesso!')
     } catch (error) {
       errorMessage('Não foi realizar a edição da unidade!')
       window.onerror(`editBranch: ${error.error}`, window.location.href)
-
     }
-  }
-
-  const handleSelectedBranch = driver => {
-    setbranchSelected(driver)
   }
 
   const handleFilter = () => {
@@ -95,66 +82,75 @@ const Manager = ({
 
     const isValidCnpj = cnpj.isValid(searchValue)
     let searchLocal = `?name=${searchValue}`
-    let query = { name: searchValue }
-    
+
     if (isValidCnpj) {
-      searchLocal = `?document=${searchValue}`
-      query = { document: searchValue.replace(/\D/g, '')}
+      searchLocal = `?document=${searchValue.replace(/\D/g, '')}`
     }
 
     localStorage.setItem('branchSearch', searchValue)
 
-    history.push({
-      pathname,
-      search: searchLocal
-    })
-
-    getBranchs(query)
-    
+    changeQueryParams(searchLocal)
   }
 
-  const handleFilterOnchange = value => {
+  const handleFilterOnchange = (value) => {
     setSearchValue(value.target.value)
   }
 
-  const clearFilter = () => {
-    setSearchValue('')
-    localStorage.removeItem('branchSearch')
-    setSearchValue('')
-    history.push({
-      pathname,
-      search: ''
-    })
-    setoffset(1)
-    getBranchs({})
+  const handleSelectedBranch = (driver) => {
+    setbranchSelected(driver)
   }
 
-  const handleChangeTableEvent = ({ current }) => {
-    setoffset(current)
-    let query = { offset: (current - 1) }
-    if (searchValue) {
-      const params = cnpj.isValid(searchValue) ? { document: searchValue.replace(/\D/g, '') } : { name: searchValue }
-      query = { ...query, ...params }
+  const handleSubmit = async (values) => {
+    try {
+      await createBranch({
+        ...values,
+        document: values.document.replace(/\D/g, '')
+      })
+      getBranchs()
+      success('Cadastro da unidade realizado com sucesso!')
+    } catch (error) {
+      errorMessage('Não foi realizar o cadastro da unidade!')
+      window.onerror(`createBranch: ${error.error}`, window.location.href)
     }
-
-    getBranchs(query)
   }
+
+  useEffect(() => {
+    const searchLocaStorage = localStorage.getItem('branchSearch')
+    if (!search && searchLocaStorage) {
+      changeQueryParams(
+        cnpj.isValid(searchLocaStorage)
+          ? `?document=${searchLocaStorage}`
+          : `?name=${searchLocaStorage}`
+      )
+
+      setSearchValue(localStorage.getItem('branchSearch'))
+    }
+  }, [])
+
+  useEffect(() => {
+    const queryParams = parseQueryParams(search)
+    const current = pipe(pathOr('0', ['offset']), Number, add(1))(queryParams)
+
+    setoffset(current)
+
+    getBranchs()
+  }, [search])
 
   return (
     <ManagerContainer
-      source={branchData}
-      loading={loading}
-      handleSubmit={handleSubmit}
-      handleSelectedBranch={handleSelectedBranch}
       branchSelected={branchSelected}
+      clearFilter={clearFilter}
+      goToDetail={(id) => history.push(`/logged/branch-detail/${id}`)}
+      handleChangeTableEvent={handleChangeTableEvent}
       handleEdit={handleEdit}
-      searchValue={searchValue}
       handleFilter={handleFilter}
       handleFilterOnchange={handleFilterOnchange}
-      clearFilter={clearFilter}
+      handleSelectedBranch={handleSelectedBranch}
+      handleSubmit={handleSubmit}
+      loading={loading}
       offset={offset}
-      handleChangeTableEvent={handleChangeTableEvent}
-      goToDetail={id => history.push(`/logged/branch-detail/${id}`)}
+      searchValue={searchValue}
+      source={branchData}
     />
   )
 }
